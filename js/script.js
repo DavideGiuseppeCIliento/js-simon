d9c707f57689ca45ebb1e20ba2013fc7b2e42003
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-11 17:30:46 +0200

    Fatto tutto il procedimento, manca il responso finale.

--- js/script.js
+++ js/script.js
@@ -3,15 +3,6 @@
 
 // ##################### FUNZIONI
 
-// ------- ANSWER DISPLAY ON
-
-const Answerdisplay = () => {
-  countdownEl.classList.add("d-none");
-  formEl.classList.replace("d-none", "d-block");
-  textInstructions.innerText = "Vediamo se riesci a ricordarli!";
-  return;
-};
-
 // ------- RANDOM NUMBER GENERATION
 
 const numberRandom = () => {
@@ -32,6 +23,15 @@ const stampNumberRandom = (numbers) => {
   }
 };
 
+// ------- ANSWER DISPLAY ON
+
+const Answerdisplay = () => {
+  countdownEl.classList.add("d-none");
+  formEl.classList.replace("d-none", "d-block");
+  textInstructions.innerText = "Vediamo se riesci a ricordarli!";
+  return;
+};
+
 // ------- ACQUISIZIONE NUMERI UTENTE
 
 const getPlayerNumbers = () => {
@@ -47,7 +47,32 @@ const getPlayerNumbers = () => {
   return numeri;
 };
 
-// ------- ACQUISIZIONE NUMERI UTENTE
+// ------- COMPARAZIONE NUMERI
+
+const compareNumber = (playerNumbers, rightNumbers) => {
+  let count = 0;
+  const okNumbers = [];
+  for (i = 0; i < playerNumbers.length; i++) {
+    if (rightNumbers.includes(playerNumbers[i])) {
+      count++;
+      okNumbers.push(playerNumbers[i]);
+    }
+  }
+
+  return [count, okNumbers];
+};
+
+// ------- CALCOLO RISULTATO FINALE
+const calcResult = (arrayResult) => {
+  const count = arrayResult[0];
+  const numberWinner = arrayResult[1];
+
+  if (count === 5) {
+    bodyEl.innerHTML = `<h1 class="display-1 text-black">HAI VINTO!</h1>`;
+  } else if (count !== 5) {
+    resultMessage.innerText = `Hai indovinato solo ${count}: numeri:${numberWinner}.`;
+  }
+};
 
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
@@ -58,22 +83,23 @@ const countdownEl = document.getElementById("countdown");
 const formEl = document.getElementById("answers-form");
 const textInstructions = document.getElementById("instructions");
 const numbersEl = document.getElementById("numbers-list");
-
+const resultMessage = document.getElementById("message");
+const bodyEl = document.querySelectorAll("body");
 // --------------------------------------------------------------------------------------
 // --------------------------------------------------------------------------------------
 
 // ##################### ALGORITMO
 
-const numbers = numberRandom();
-stampNumberRandom(numbers);
+const rightNumbers = numberRandom();
+stampNumberRandom(rightNumbers);
 
 // -------CONTDOWN
 
-let secondsTimer = 5;
+let secondsTimer = 2;
 const timer = setInterval(() => {
   countdownEl.innerText = secondsTimer;
 
-  console.log(numbers);
+  console.log(rightNumbers);
 
   secondsTimer--;
 
@@ -89,9 +115,11 @@ const timer = setInterval(() => {
 
 formEl.addEventListener("submit", function (event) {
   event.preventDefault();
-  const playerNumbers = getPlayerNumbers(numbers);
+  const playerNumbers = getPlayerNumbers(rightNumbers);
   console.log(playerNumbers);
-  compareNumber();
+  const arrayResult = compareNumber(playerNumbers, rightNumbers);
+  console.log(arrayResult);
+  calcResult();
 });
 
 // --------------------------------------------------------------------------------------
